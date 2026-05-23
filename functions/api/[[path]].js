68901ea073ef745557031175f0a385c600065955
James Theakston <jamestheakston7@outlook.com>
2026-05-23 10:44:38 +0100

    Implement API route for GET requests

---
+++ functions/api/[[path]].js
@@ -0,0 +1,38 @@
+export async function onRequestGet(context) {
+
+    try {
+
+        const path =
+            context.params.path.join("/");
+
+        const response = await fetch(
+            `https://www.classcharts.com/apiv2student/${path}`,
+            {
+                headers: {
+                    Authorization:
+                        context.request.headers.get(
+                            "Authorization"
+                        )
+                }
+            }
+        );
+
+        const data =
+            await response.json();
+
+        return Response.json(data);
+
+    } catch(err){
+
+        return Response.json(
+            {
+                error: err.message
+            },
+            {
+                status: 500
+            }
+        );
+
+    }
+
+}
